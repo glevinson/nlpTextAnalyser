f49092734479d3c22dfaebadae962afd16398ad3
Matt Barker <105945282+m-barker@users.noreply.github.com>
2022-07-06 16:44:41 +0100

    Remove theme

--- AppClass.py
+++ AppClass.py
@@ -96,49 +96,9 @@ class App:
 
 def runApp():
     root = Tk()
-    setTheme(root)
     App(root)
     root.mainloop()
     
-def setTheme(root):
-    """
-    Theme from https://github.com/rdbende/Azure-ttk-theme 
-    MIT License
-
-    Copyright (c) 2021 rdbende
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
-    """
-    try:
-        root.tk.call("source","Azure-ttk-theme-main/azure.tcl")
-    except:
-            try:
-                root.tk.call("source","Azure-ttk-theme-main\\azure.tcl")
-            except:
-                print("Error loading theme")
-        
-    root.tk.call("set_theme","light") 
-
-
-
-
-
 if  __name__ == "__main__":
     runApp()
 
